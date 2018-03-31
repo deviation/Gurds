e95329562d106073c4a67f786b66aa5ae6d97da6
Daniel White <dan.white@salesforce.com>
2018-03-30 23:16:03 -0300

    Adding user/employee login logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,29 +3,30 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = Customer.find_by(Email: params[:session][:email])
+    if !user
+      user = Employee.find_by(Email: params[:session][:email])
+    end
 
-    customer = Customer.find_by(Email: params[:session][:email])
-    
-puts "customer password: " + customer.password_digest
-    if customer.authenticate(params[:session][:password])
-      puts "Customer Logged in: " + params[:session][:email]
-            # Log the user in and redirect to the user's show page.
-      log_in customer
-      redirect_to customer_path(customer)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      log_in user
+      if user.is_a?(Customer)
+        redirect_to customer_path(user)
+      else 
+ 
+      end
 
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
     end
-
   end
 
   def destroy
-
-    log_out
-    redirect_to root_url
-
+      log_out
+      redirect_to root_url
   end
 
 end
